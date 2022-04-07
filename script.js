b54ba371c148e497dba3f1908fefad5a454af686
ACV33 <ashli.vaccaro@gmail.com>
2022-04-07 11:47:56 -0400

    updated script.js
    Added a pokemon list array

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
-alert('Hello world');
-var favoriteFood= 'Tacos';
-document.write('Tacos');
+let pokemonList = [
+  {name: "pikachu", height: "2", type: "electric"},
+  {name: "charizard", height: "6" type: "fire", "flying"},
+  {name: "eevee", height: "1", type: "normal" }
+];
