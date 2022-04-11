4c341e912a88780d3b52a92906bc6a3dbc2dd536
ACV33 <ashli.vaccaro@gmail.com>
2022-04-11 19:30:13 -0400

    testing code

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
-alert('Hello world');
+let pokemonList = [
+  {name: "pikachu", height: "2", type: "electric"},
+  {name: "charizard", height: "6" type: "fire", "flying"},
+  {name: "eevee", height: "1", type: "normal" }
+  document.write("pikachu");
+];
+
+let text = "";
+let i = 0;
+for (;pokemonList[i];){
+  text = text + " " + pokemonList[i];
+  i++;
+}
--- script.js
+++
@@ -1,5 +0,0 @@
-let pokemonList = [
-  {name: "pikachu", height: "2", type: "electric"},
-  {name: "charizard", height: "6" type: "fire", "flying"},
-  {name: "eevee", height: "1", type: "normal" }
-];
