051c5e6fbcbe648651fddcfcf4d2fed2a3f7e43c
ACV33 <ashli.vaccaro@gmail.com>
2022-04-11 18:37:56 -0400

    added language
    Added language to html doc

---
+++ script.js
@@ -0,0 +1,5 @@
+let pokemonList = [
+  {name: "pikachu", height: "2", type: "electric"},
+  {name: "charizard", height: "6" type: "fire", "flying"},
+  {name: "eevee", height: "1", type: "normal" }
+];
