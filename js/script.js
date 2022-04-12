609a8bf33ed8ada37641d82dfccebbd6a63da478
ACV33 <ashli.vaccaro@gmail.com>
2022-04-12 18:57:54 -0400

    Added document write
    Added document.write to have pokemon list output the javascript file

--- js/script.js
+++ js/script.js
@@ -1,19 +1,19 @@
 let pokemonList = [
   {name: "pikachu",
   height: "2",
-  types: "electric
-  "},
+  types: "electric"
+},
   {name: "charizard",
   height: "6",
-  types: "fire",
-  "flying"},
+  types: "fire"
+},
   {name: "eevee",
   height: "1",
   types: "normal" }
 ];
 
 for (const pokemon of pokemonList) {
-  document.write("pikachu (height: 2)");
-  document.write("charizard (height: 6)")
-  document.write("eevee (height: 1)")
+  document.write("pikachu" + " " + "height: 2");
+  document.write("charizard" + " " + "height: 6");
+  document.write("eevee" + " " + "height: 1");
 }
