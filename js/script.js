4ce880df770a0920479a8aa114708f33ee204e92
ACV33 <ashli.vaccaro@gmail.com>
2022-04-28 13:49:57 -0400

    updated script.js with forEach
    added forEach() to pokemon list

--- js/script.js
+++ js/script.js
@@ -14,10 +14,21 @@ let pokemonList = [
   types: "normal" }
 ];
 
-for (const pokemon of pokemonList) {
-  if (pokemon.height > 5){
-    document.write (pokemon.name + " that's a big Pokemon!");
-  } else{
-    document.write(`<p>${pokemon.name} is ${pokemon.height} feet tall and is a ${pokemon.types} type.</p>`);
-  }
-}
+let pokemonList2 = [
+  {name: "Jigglypuff"}
+]
+// A conditonal that lists the pokemons out with there height and type
+// function printArrayDetails(list){
+// for (const pokemon of pokemonList) {
+ // if (pokemon.height > 5){
+ //  document.write (pokemon.name + " that's a big Pokemon!");
+//  } else{
+//    document.write(`<p>${pokemon.name} is ${pokemon.height} feet tall and is a ${pokemon.types} type.</p>`);
+//  }}}
+
+//printArrayDetails(pokemonList);
+//printArrayDetails(pokemonList2);
+
+pokemonList.forEach(function(pokemon) {
+  document.write(pokemon.name + ' is ' + pokemon.height + ' and is a ' + pokemon.types + 'type.');
+});
